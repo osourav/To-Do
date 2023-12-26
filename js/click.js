a51b7307a14e3aab9890fc338efd256b7a043822
Sourav Barui <74421679+im-sourav@users.noreply.github.com>
2023-12-26 17:11:00 +0530

    update css

--- js/click.js
+++ js/click.js
@@ -1,8 +1,15 @@
 const nav_btn = document.querySelectorAll(".nav-btn:not(.up)");
-
 nav_btn.forEach(btn => {
    btn.addEventListener("click", () => {
       nav_btn.forEach(btn => btn.classList.remove("active"));
+      btn.classList.add("active");
+   })
+});
+
+const home_category = document.querySelectorAll(".category");
+home_category.forEach(btn => {
+   btn.addEventListener("click", () => {
+      home_category.forEach(btn => btn.classList.remove("active"));
       btn.classList.add("active");
    })
 });
